e06ad6a2fd62cd8cdb13e6810d3a2c43af477fc4
J Matti <JMattiDev@gmail.com>
2020-03-14 13:04:52 +1100

    changed images to refresh instead of stack

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ const getPugs = function() {
   .then((data) => {
     const pugContainer = document.querySelector('.pug-container')
     data.message.forEach(img => {
-      pugContainer.innerHTML += `<img src='${img}'>` 
+      pugContainer.innerHTML += `<img class='pug-img' src='${img}'>` 
+    });
+  });
+}
+
+const refreshPugs = function() {
+  fetch('https://dog.ceo/api/breed/pug/images/random/3')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const pugImg = document.querySelectorAll('.pug-img')
+    pugImg.forEach((img, index) => {
+      img.src = data.message[index]
     });
   });
 }
 
 document.addEventListener('DOMContentLoaded', getPugs)
-document.getElementById('pug-refresh').addEventListener('click', getPugs)
+document.getElementById('pug-refresh').addEventListener('click', refreshPugs)
